65fac048119b236b9ab4917ccde7bfb110c8a4dd
Mannatu <mannatu@mannatu.com>
2022-07-17 02:25:36 +0200

    the scope of this update is beyond a hotfix

--- Neurotrauma/Lua/Autorun/init.lua
+++ Neurotrauma/Lua/Autorun/init.lua
@@ -2,8 +2,8 @@
 
 NT = {} -- Neurotrauma
 NT.Name="Neurotrauma"
-NT.Version = "A1.7.9h5"
-NT.VersionNum = 01070905
+NT.Version = "A1.7.10"
+NT.VersionNum = 01071000
 NT.Path = table.pack(...)[1]
 
 dofile(NT.Path.."/Lua/Scripts/helperfunctions.lua")
