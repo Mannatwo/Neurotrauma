3bc87456a1e470f8e8acdd702f6dbbd982f40a36
Mannatu <mannatu@mannatu.com>
2022-07-19 18:04:26 +0200

    NTSP surgical infection config

--- NT Surgery Plus/Lua/Scripts/humanupdate.lua
+++ NT Surgery Plus/Lua/Scripts/humanupdate.lua
@@ -35,7 +35,7 @@ function NTSP.PreUpdateHuman(character)
     end
 
     -- sterility during surgery
-    if HF.HasAffliction(character,"surgeryincision") then
+    if NT.Config.NTSPenableSurgicalInfection and HF.HasAffliction(character,"surgeryincision") then
 
         local sterility = 50
 
