2492fcbb73a896bc1a6c1d5c4aeb7cab58a5965c
Mannatu <mannatu@mannatu.com>
2023-02-01 02:14:51 +0100

    fixing selfish medicine application

--- Neurotrauma/Lua/Autorun/init.lua
+++ Neurotrauma/Lua/Autorun/init.lua
@@ -2,8 +2,8 @@
 
 NT = {} -- Neurotrauma
 NT.Name="Neurotrauma"
-NT.Version = "A1.8.2h2"
-NT.VersionNum = 01080202
+NT.Version = "A1.8.2h3"
+NT.VersionNum = 01080203
 NT.Path = table.pack(...)[1]
 
 -- config loading
