39ecf40e0362e3a589d8b782100df43df5fd8ae6
Mannatu <mannatu@mannatu.com>
2023-11-10 19:30:16 +0100

    version number change

--- Neurotrauma/Lua/Autorun/init.lua
+++ Neurotrauma/Lua/Autorun/init.lua
@@ -2,8 +2,8 @@
 
 NT = {} -- Neurotrauma
 NT.Name="Neurotrauma"
-NT.Version = "A1.8.6"
-NT.VersionNum = 01080503
+NT.Version = "A1.8.6h1"
+NT.VersionNum = 01080601
 NT.Path = table.pack(...)[1]
 
 -- config loading
